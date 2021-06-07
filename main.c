30cf60b2a6c05c90b6bc2020f6b44b5382b98fe4
RolandGamos <lucasweglarz9@gmail.com>
2021-06-08 00:48:46 +0200

    main

---
+++ main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/19 12:46:42 by user42            #+#    #+#             */
+/*   Updated: 2021/06/07 21:06:26 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/push_swap.h"
+
+int main(int ac, char **av)
+{
+	t_stack	stack;
+
+	init_stack(&stack, av, ac);
+	fill_stack(&stack);
+	if (check_sorting(stack.stack_a, stack.stack_a_len) == 1)
+	{
+		free(stack.stack_a);
+		return (0);
+	}
+	if (stack.stack_a_len == 2)
+		do_sa(&stack);
+	if (stack.stack_a_len == 3)
+		sort_stack_3(&stack);
+	if (stack.stack_a_len == 5)
+	{
+		sort_stack_5(&stack);
+		while (stack.stack_b_len)
+			do_instruction1(&stack ,"pa");
+		free(stack.stack_b);
+	}
+	if (stack.stack_a_len > 5)
+		sort_long_stack(&stack);
+	free(stack.stack_a);
+}
