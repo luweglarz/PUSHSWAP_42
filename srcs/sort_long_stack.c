7780c6fe66642d80aef94d3486a7ba7f44a568e9
RolandGamos <lucasweglarz9@gmail.com>
2021-07-28 22:40:46 +0200

    Merge sort done, time to norme the project

--- srcs/sort_long_stack.c
+++ srcs/sort_long_stack.c
@@ -12,6 +12,8 @@
 
 #include "../includes/push_swap.h"
 
+#include "../includes/push_swap.h"
+
 int	nb_sup_a(t_stack *stack)
 {
 	int	i;
@@ -28,37 +30,17 @@ int	nb_sup_a(t_stack *stack)
 	return (j);
 }
 
-int	nb_inf_b(t_stack *stack)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (i < stack->stack_b_len)
-	{
-		if (stack->stack_b[i] < stack->stack_b[i + 1])
-			j++;
-		i++;
-	}
-	return (j);
-}
-
 void	split_stack(t_stack *stack)
 {
-	int	stack_a;
-	int	stack_b;
+	int	nb_sup;
 
-	stack_a = nb_sup_a(stack);
-	stack_b = nb_inf_b(stack);
+	nb_sup = nb_sup_a(stack);
 	while (stack->stack_a[0] > stack->stack_a[stack->stack_a_len - 1])
-			do_instruction1(stack, "rra");
-	while (stack_a + 1 > stack_b)
-	{
+ 			do_instruction1(stack, "rra");
+	if (check_sorting(stack->stack_a, stack->stack_a_len) == 1)
+		return ;
+	while (stack->stack_a_len != nb_sup)
 		do_instruction2(stack, "pb");
-		stack_a = nb_sup_a(stack);
-		stack_b = nb_inf_b(stack);
-	}
 }
 
 void	sort_long_stack(t_stack *stack)
@@ -68,11 +50,6 @@ void	sort_long_stack(t_stack *stack)
 		split_stack(stack);
 		while (stack->stack_b_len != 0)
 		{
-			  for (int i = 0; i < stack->stack_a_len; i++)
-			  	printf("\rstack_a[%d] %d\n",i, stack->stack_a[i]);
-			  printf("\n");
-			  for (int i = 0; i < stack->stack_b_len; i++)
-			  	printf("\rstack_b[%d] %d\n",i, stack->stack_b[i]);
 			if (stack->stack_b[0] < stack->stack_a[0] && stack->stack_b[0]
 			> stack->stack_a[stack->stack_a_len - 1])
 				do_instruction2(stack, "pa");
@@ -90,3 +67,4 @@ void	sort_long_stack(t_stack *stack)
 			break ;
 	}
 }
+
